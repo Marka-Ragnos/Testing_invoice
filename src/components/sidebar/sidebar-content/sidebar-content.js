1fe63743cecf868ad09bdc51fa26c3e6427a1527
Саня <dunkan-1@yandex.ru>
2021-01-13 13:43:07 +0300

    added username

--- src/components/sidebar/sidebar-content/sidebar-content.js
+++ src/components/sidebar/sidebar-content/sidebar-content.js
@@ -1,17 +1,18 @@
 import React from "react";
 import { connect } from "react-redux";
 import { Container, Row, Col, Image, Nav } from "react-bootstrap";
-import { getAvatar } from "../../../store/user/selectors";
+import { getAvatar, getUserName } from "../../../store/user/selectors";
 import Navigation from "../../navigation/navigation";
 
-const SidebarContent = ({ isOpen, avatar }) => {
+const SidebarContent = ({ isOpen, avatar, name }) => {
    return (
       <Container
          className={`sidebar-content ${isOpen ? "sidebar-content--open" : ""}`}
       >
          <Row className="justify-content-center avatar">
             <Col xs={5} md={8}>
-               <Image src={avatar} roundedCircle fluid />
+					<Image src={avatar} roundedCircle fluid />
+					<p className="avatar__name">{name}</p>
             </Col>
          </Row>
          <Nav
@@ -28,6 +29,7 @@ const SidebarContent = ({ isOpen, avatar }) => {
 
 const mapStateToProps = (state) => ({
    avatar: getAvatar(state),
+   name: getUserName(state),
 });
 
 export default connect(mapStateToProps)(SidebarContent);
--- src/store/user/selectors.js
+++ src/store/user/selectors.js
@@ -6,4 +6,9 @@ export const getAuthorizationStatus = (state) =>
 export const getAvatar = (state) =>
    state[NameSpace.USER].gitHubData.avatar
       ? state[NameSpace.USER].gitHubData.avatar
-      : `images/avatar-primer.jpg`;
+		: `images/avatar-primer.jpg`;
+		
+export const getUserName = (state) =>
+   state[NameSpace.USER].gitHubData.name
+      ? state[NameSpace.USER].gitHubData.name
+      : `User Name`;
